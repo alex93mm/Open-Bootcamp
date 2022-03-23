3974c0e33f7708eb416d05dbf49724dfff938986
Alejandro Martinez <alejandro93mm@gmail.com>
2022-03-23 12:24:38 +0100

    Exceptions UPDATED & CREATED - Created Unathorized exception and modified Handler

--- Second Commit/Fase Final/Backend/app/Exceptions/Handler.ts
+++ Second Commit/Fase Final/Backend/app/Exceptions/Handler.ts
@@ -1,23 +1,21 @@
-/*
-|--------------------------------------------------------------------------
-| Http Exception Handler
-|--------------------------------------------------------------------------
-|
-| AdonisJs will forward all exceptions occurred during an HTTP request to
-| the following class. You can learn more about exception handling by
-| reading docs.
-|
-| The exception handler extends a base `HttpExceptionHandler` which is not
-| mandatory, however it can do lot of heavy lifting to handle the errors
-| properly.
-|
-*/
-
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
     super(Logger)
+  }
+
+  public async handle(error, ctx: HttpContextContract) {
+    if (error.code === 'E_ROW_NOT_FOUND') {
+      return ctx.response.notFound({ message: 'No encontrado' })
+    }
+
+    if (error.code === 'E_UNATHORIZED_ACCESS') {
+      return ctx.response.unauthorized({ message: 'No autenticado' })
+    }
+
+    return super.handle(error, ctx)
   }
 }
